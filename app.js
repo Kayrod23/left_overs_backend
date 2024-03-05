23845488cf6bc4f222f5155ff6473ae66ff4dfec
Keeanu Rodriguez <keeanu7@gmail.com>
2024-03-04 20:53:16 -0500

    testing aws

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to left Overs');
+    res.send('Welcome to left Overs Backend');
 })
 
 app.use("/users", users);
