b987356f0a8ff183a90f9c234eba679acfe573d1
Keeanu Rodriguez <keeanu7@gmail.com>
2024-03-05 19:13:00 -0500

    test failed

--- app.js
+++ app.js
@@ -20,24 +20,4 @@ app.get("*", (req, res) => {
     res.status(404).json({error: "Not Found!"});
 });
 
-var pg = require('pg');
-
-var connection = pg.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to database.');
-});
-
-connection.end();
-
 module.exports = app;
